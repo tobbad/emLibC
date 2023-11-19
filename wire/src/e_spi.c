ee59fad0ebeaece0cc4e20e89d1218f423aac9ff
Tobias Badertscher <tobbad@gmail.com>
2023-11-19 15:23:02 +0100

    Work in progress

---
+++ wire/src/e_spi.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *
+ *    W a v e L a b
+ *
+ *    Engineering AG
+ *
+ *    Project:          Radio Bell
+ *
+ *    Programmer:		Tobias Badertscher
+ *
+ *    Date:				5.10.2023
+ *
+ *    File name   :
+ *    Description :
+ *                      used for:
+ *                      -
+ *                      -
+ *                      -
+ *
+ **************************************************************************/
+#include "main.h"
+#include "common.h"
+#include "lspi.h"
+spi_t spi_init(void * spi_dev, spi_t *spi, GpioPin_t *sel_pin, uint8_t cpol, uint8_t cpha){
+	spi_t ret;
+	return ret;
+}
+
+elres_t spi_readWrite(spi_t *spi_dev, int16_t adr, spi_dir dir, uint8_t cnt, uint8_t *){
+	return EMLIB_OK;
+
+}
+
+elres_t spi_writeCmd(spi_t *spi_dev, uint8_t cmd){
+	return EMLIB_OK;
+
+}
