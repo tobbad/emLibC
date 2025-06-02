23242e9c8b831fe2fba01e5dda0e95e3ea32240c
Tobias Badertscher <tobias.badertscher@baerospace.ch>
2025-06-02 22:17:32 +0200

    Reset terminal

--- common/src/terminal.c
+++ common/src/terminal.c
@@ -4,42 +4,33 @@
  *  Created on: Jun 10, 2024
  *      Author: TBA
  */
+#include "main.h"
 #include "common.h"
 #include "device.h"
 #include "state.h"
-#include "serial.h"
 #include <keyboard.h>
 
-typedef struct termnal_dev_s{
-	sio_t *sio;
-	state_t state;
-	dev_handle_t dev;
-}terminal_dev_t;
-
-static terminal_dev_t my_term;
-static state_t dev_state = {
-    .first = 1, //First valid value
-    .cnt =8,
+static state_t my_term = {
+    .first = 0, //First valid value
+    .cnt =9,
     .state  = { OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF },
     .label =  { 'R', '1', '2', '3', '4', '5', '6', '7', '8' },
 };
-static uint8_t ch=0xff;
-static bool rxData = false;
+
 static bool check_key(char ch);
 static void terminal_reset(dev_handle_t dev, bool hard);
 
-static void terminal_init(dev_handle_t handle, dev_type_e dev_type,	sio_t *sio) {
+static void terminal_init(dev_handle_t handle, dev_type_e dev_type,	xpad_t *device) {
 	terminal_reset(handle, true);
-	my_term.state = dev_state;
-	my_term.sio = sio;
 }
 
 static bool check_key(char ch) {
     bool ret = false;
-    return state_ch2idx(&my_term.state, ch)>=0;
+    return state_ch2idx(&my_term, ch)>=0;
 }
 
 static uint16_t terminal_scan(dev_handle_t dev) {
+    char ch;
 	static bool asked = false;
 	uint16_t res = 0;
 	//char allowed_keys={'R'};
@@ -49,35 +40,34 @@ static uint16_t terminal_scan(dev_handle_t dev) {
 		printf("Please enter key"NL);
 	}
     HAL_StatusTypeDef status;
-    rxData = false;
-    HAL_UARTEx_ReceiveToIdle_IT(my_term.sio->uart,&ch, LINE_LENGTH);
-	while (rxData == false){}
-	rxData = false;
-	ch = toupper(ch);
-	int8_t idx=state_ch2idx(&my_term.state, ch);
-	if (idx>=0){
-		res = ch - '0';
-		if (res<9){
-			state_propagate(&my_term.state, ch);
-			my_term.state.clabel = 0;
-		} else {
-			my_term.state.clabel = ch;
-			my_term.state.state[0] = ON;
-		}
+    status = HAL_UART_Receive(&huart2, (uint8_t*)&ch, 1, 0);
+    if (status == HAL_OK) {
+        ch = toupper(ch);
+        int8_t idx=state_ch2idx(&my_term, ch);
+        if (idx>=0){
+            res = ch - '0';
+            if (res<9){
+                state_propagate(&my_term, ch);
+                my_term.clabel = 0;
+            } else {
+                my_term.clabel = ch;
+            }
+        }
+        asked = false;
+        return res;
 	}
-	asked = false;
 	return res;
 }
 
 static void terminal_state(dev_handle_t dev, state_t *ret) {
-    state_merge(&my_term.state, ret);// problem here
+    state_merge(&my_term, ret);// problem here
 }
 
 static void terminal_reset(dev_handle_t dev, bool hard) {
-    my_term.state.dirty=false;
+    my_term.dirty=false;
     if (hard){
-        for (uint8_t i = my_term.state.first; i < my_term.state.first + my_term.state.cnt; i++) {
-            my_term.state.state[i] = OFF;
+        for (uint8_t i = my_term.first; i < my_term.first + my_term.cnt; i++) {
+            my_term.state[i] = OFF;
         }
     }
 	return;
@@ -95,24 +85,29 @@ kybd_t terminal_dev = {
 };
 
 int8_t terminal_waitForNumber(char **key) {
-	static uint8_t buffer[LINE_LENGTH];
+	static char buffer[LINE_LENGTH];
 	memset(buffer, 0, LINE_LENGTH);
 	HAL_StatusTypeDef status;
+	uint8_t ch = 0xFF;
 	bool stay=true;
 	int16_t idx = 0;
 	while ((ch == 0xff)&&(stay)) {
-		status = HAL_UARTEx_ReceiveToIdle_IT(my_term.sio->uart, (uint8_t*)&ch, LINE_LENGTH);
-		while (rxData == false){}
-		rxData = false;
-		if (((ch >= '0') && (ch < '9')) || (ch == 'R')|| (ch=='r') || (ch == '+') || (ch == '-')) {
-			buffer[idx++] = ch;
-			if ((ch == 'R')|| (ch=='r')){
-				stay = false;
-			}
-			if (idx>=1){
-				stay = false;
-			}
-			ch = 0xFF;
+		status = HAL_UART_Receive(&huart2, &ch, 1, 0);
+		if (status == HAL_OK){
+	        if (ch == '\r'){
+	        	stay = false;
+	        	ch=0xff;
+	        }
+            if (((ch >= '0') && (ch < '9')) || (ch == 'R')|| (ch=='r') || (ch == '+') || (ch == '-')) {
+                buffer[idx++] = ch;
+                if ((ch == 'R')|| (ch=='r')){
+                	stay = false;
+                }
+                if (idx>=1){
+                	stay = false;
+                }
+                ch = 0xFF;
+            }
 		}
 	}
 	char *stopstring = NULL;
@@ -126,10 +121,4 @@ int8_t terminal_waitForNumber(char **key) {
 		return res;
 	}
 	return -1;
-}
-
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
-{
-  rxData= true;
-  HAL_UARTEx_ReceiveToIdle_IT(my_term.sio->uart, (uint8_t*)&ch, 1);
 }
