ba81f828fffa50205a4b5e676a41f51339997232
Tobias Badertscher <tobias.badertscher@wavelab.ch>
2025-06-16 12:44:54 +0200

    WIP

--- common/inc/state.h
+++ common/inc/state.h
@@ -20,26 +20,35 @@ typedef enum{
 }key_state_e;
 
 typedef union {
-     uint32_t cmd;  // Kann ein pointer zu einem Pointer enthalten, das den anderen Geräten mitgeteilt wird oder NULL
+     uint32_t cmd;  // Kann ein pointer zu einem Pointer enthalten, das
+                    // den anderen Geräten mitgeteilt wird oder NULL
      char str[CMD_LEN]; //CMD_LEN ist 4 ist leer (0) oder ein command
  }clabel_u;
 
 typedef struct state_s{
     uint8_t first;
     uint8_t cnt;
-    bool dirty;     // Evtl. können in den obersten 2 bit der Inhalt des clabel fields encodiert werden (01: cmd, 11:str))
+    bool dirty;     // Evtl. können in den obersten 2 bit der Inhalt des clabel fields
+                    // encodiert werden (01: cmd, 11:str))
     clabel_u clabel;
     key_state_e state[MAX_BUTTON_CNT];
     char  label[MAX_BUTTON_CNT];
 } state_t; // Size is 2*MAX_BUTTON_CNT + 4=  36 Byte (MAX_BUTTON_CNT = 16)
 
+typedef union {
+     uint32_t cmd;  // Kann ein pointer zu einem Pointer enthalten, das
+                    // den anderen Geräten mitgeteilt wird oder NULL
+     char str[CMD_LEN]; //CMD_LEN ist 4 ist leer (0) oder ein command
+ }clabel_u;
+
 typedef struct statea_s{
-    uint8_t range;  // Unteres Nibble: tiefstes gültiges Labele (0-15);Oberes Nibble: letzes gültiges Label (0-15)
+    uint8_t range;  // Unteres Nibble: tiefstes gültiges Labele (0-15):
+                    // Oberes Nibble: letzes gültiges Label (0-15)
                     // und daher hat ein Label nur 2 Bit in den 16 Bit state Variable
     bool dirty;     // Das zwei Topbit indizieren die Art der clabel (01: *cmd, 11; str[CMD_LEN])
     uint32_t state; // Jedes label hat eine  state: 00=OFF, 01: BLINKING, 11 ON 16*2 =32 Bit
     clabel_u clabel;// 4 Bytes
-} statea_t; // Size is 10 Bytes, Label gibt es nicht da sowiso MAX_BUTTON_CNT (0..MAX_BUTTON_CNT-1) Labels gibt
+} statea_t; // Size is 10 Bytes, Label gibt es nicht da es owiso MAX_BUTTON_CNT (0..MAX_BUTTON_CNT-1) Labels gibt
 
 int8_t state_ch2idx(state_t *state, char ch);
 void state_init(state_t *state);
