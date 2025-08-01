6d0f0650b4d5ca14becb491924907bac17dd124e
Tobias Badertscher <tobias.badertscher@baerospace.ch>
2025-08-01 17:26:40 +0200

    WIP

--- common/inc/buffer_pool.h
+++ common/inc/buffer_pool.h
@@ -12,23 +12,19 @@
 #define BUFFER_CNT 20
 typedef bp_type_e {
 	POOL,  // Filled with 0
-	LIFO,  // Can be used
-	FIFO,  // Data in it, can be used (means dirty)
+	RING,  // Ringbuffer
 };
 
 typedef buffer_pool_s{
 	uint8_t bufer_cnt;
-	buffer_t *buffer;
+	buffer_t *buffer[BUFFER_CNT];
 	bp_type_e type;
+	uint8_t next;
 }buffer_pool_t;
 
-buffer_pool_t* buffer_pool_new(uint8_t cnt, bp_type_e type){
-	buffer_pool_t * bp;
-	bp = malloc(sizeof(buffer_pool_t));
-	bp->type= type;
-	return bp;
-}
-
-em_msg buffer_pool_add(buffer_pool_t bp, buffer_t *buffer)
+buffer_pool_t* buffer_pool_new(uint8_t cnt, bp_type_e type);
+em_msg buffer_pool_add(buffer_pool_t *bp, buffer_t *buffer);
+buffer_t * buffer_pool_get(buffer_pool_t *bp, uint8_t index);
+em_msg buffer_pool_set(buffer_pool_t *bp, uint8_t index, buffer_t *buffer);
 
 #endif /* EMLIBC_COMMON_INC_BUFFER_POOL_H_ */
--- common/src/buffer.c
+++ common/src/buffer.c
@@ -6,23 +6,22 @@
  */
 #include "buffer.h"
 
-buffer_t * buffer_init(buffer_t *buffer, uint16_t size, bool doAlloc){
-	if (buffer->mem!=NULL){
+buffer_t * buffer_init(buffer_t *buffer, uint16_t size){
+	if (buffer!=NULL){
+		buffer->size = size;
 		buffer->mem = malloc(buffer->size);
+		buffer->pl = buffer->mem;
+		memset(buffer->mem, 0, buffer->size);
+		buffer->state = ZERO;
+		buffer->user =0;
+		memset(buffer->mem, 0, buffer->size);
 	}
-	buffer->pl = buffer->mem;
-	memset(buffer->mem, 0, buffer->size);
 	return buffer;
 }
 
 buffer_t * buffer_new(uint16_t size){
 	buffer_t *buffer = malloc(sizeof(buffer_t));
 	memset(buffer , 0, sizeof(buffer_t));
-	buffer->size = size;
-	if (size>0){
-		buffer->mem = malloc(size);
-		buffer->pl = buffer->mem;
-		memset(buffer->mem, 0, size);
-	}
+	buffer_init(buffer, size);
 	return buffer;
 }
