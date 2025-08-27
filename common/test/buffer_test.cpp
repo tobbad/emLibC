f57e312abe1e7be4f766f7ef2bb5cd4b8170d011
Tobias Badertscher <tobias.badertscher@baerospace.ch>
2025-08-27 22:48:35 +0200

    Buffer is WIP

--- common/test/buffer_test.cpp
+++ common/test/buffer_test.cpp
@@ -6,17 +6,18 @@
  */
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 #include "buffer.h"
 #include "buffer_pool.h"
 
 #define BUFFER_SIZE 96
-#define BUFFER_CNT 20
+#define BUFFER_CNT 5
 
 #include "gtest/gtest.h"
 uint8_t wbuffer[BUFFER_SIZE];
 uint8_t sbuffer[BUFFER_SIZE];
-uint8_t rBufSize= 42;
-uint8_t rBufCnt= 5;
+uint8_t rBufLine= 5;
+uint8_t rBufCharCnt= 42;
 class BufferTest : public ::testing::Test {
     protected:
 
@@ -28,7 +29,7 @@ class BufferTest : public ::testing::Test {
     }
 
     void TearDown() {
-        for (uint8_t i=0;i<rBufSize;i++){
+        for (uint8_t i=0;i<rBufCharCnt;i++){
             EXPECT_EQ(wbuffer[i], i);
         }
     }
@@ -40,15 +41,16 @@ TEST_F(BufferTest, ErrorIfBufferAllocIsOK){
 	buffer_t * buf;
 	buf= buffer_new(0);
 	EXPECT_EQ(buf, (buffer_t *)NULL);
-    buf= buffer_new(rBufSize);
+    buf= buffer_new(rBufCharCnt);
     EXPECT_NE(buf, (buffer_t *)NULL);
 }
 
 TEST_F(BufferTest, SetDataToBufferAndReadItBack){
 //TEST_F(BufferTest, DISABLED_SetDataToBufferAndReadItBack){
-	const uint8_t size = rBufSize;
+	const uint8_t size = rBufCharCnt;
 	uint8_t rsize = size;
     buffer_t * buf= buffer_new(size);
+    buffer_print(buf, 0);
     em_msg res = buffer_set(buf, (uint8_t*)&wbuffer, BUFFER_SIZE);
     EXPECT_EQ(res, EM_OK);
     res = buffer_set(buf,(uint8_t*) &wbuffer, size);
@@ -63,10 +65,12 @@ TEST_F(BufferTest, SetDataToBufferAndReadItBack){
 }
 
 TEST_F(BufferTest, CreateBufferPool){
-	buffer_pool_t * pool = buffer_pool_new(rBufCnt, rBufSize);
+	buffer_pool_t * pool = buffer_pool_new(rBufLine, rBufCharCnt);
+	buffer_pool_print(pool);
 	buffer_t *buffer;
-	for (uint8_t i=0;i<rBufSize;i++){
+	for (uint8_t i=0;i<rBufLine;i++){
 		buffer = buffer_pool_get(pool);
+		buffer_print(buffer, i);
 		EXPECT_NE(buffer, (buffer_t *)NULL);
 	}
 	buffer = buffer_pool_get(pool);
