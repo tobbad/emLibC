485cb57615cea72b921c8b6a38d5e486da9110db
Tobias Badertscher <tobias.badertscher@baerospace.ch>
2026-01-07 12:36:13 +0100

    Unify attribut log

--- common/src/state.c
+++ common/src/state.c
@@ -291,10 +291,11 @@ em_msg state_print(const state_t *state, const char *title) {
         printf("Do not print corrupted payload" NL);
         return res;
     }
-    printf("first : %d" NL, state->first);
-    printf("cnt   : %d" NL, state->cnt);
-    printf("clabel: 0x%08lx" NL, state->clabel.cmd);
-    printf("label : ");
+    printf("state_t = %d" NL, sizeof(state_t));
+    printf("first   = %d" NL, state->first);
+    printf("cnt     = %d" NL, state->cnt);
+    printf("clabel  = %c" NL, state->clabel.cmd);
+    printf("label   = ");
     for (uint8_t i = 0; i < MAX_BUTTON_CNT; i++) {
         char c = state->label[i];
         if (isprint(c)) {
@@ -303,7 +304,7 @@ em_msg state_print(const state_t *state, const char *title) {
             printf("....");
         }
     }
-    printf(NL "State : ");
+    printf(NL "State = ");
     for (uint8_t i = 0; i < MAX_STATE_CNT; i++) {
         printf("%s ", key2char[state->state[i]]);
     }
